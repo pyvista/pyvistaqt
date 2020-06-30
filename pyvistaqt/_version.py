3cd7bcfce5000c38541ac7fe4ec15a8081c2f4ad
banesullivan <banesulli@gmail.com>
2020-06-30 12:32:02 -0400

    Bump version: 0.1.0 -> 0.1.1

--- pyvistaqt/_version.py
+++ pyvistaqt/_version.py
@@ -1,6 +1,6 @@
 """Version info for pyvistaqt."""
 # major, minor, patch
-version_info = 0, 1, 0
+version_info = 0, 1, 1
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
