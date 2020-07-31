0c2678ff151577cd401c1929806faa617a0a3cf9
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-07-31 12:10:26 +0200

    Make isort (5.2.2)

--- pyvistaqt/plotting.py
+++ pyvistaqt/plotting.py
@@ -45,17 +45,16 @@ import warnings
 from functools import wraps
 
 import numpy as np
+import pyvista
 import scooby
 import vtk
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import QTimer, pyqtSignal
 from PyQt5.QtWidgets import QAction, QFrame, QMenuBar, QVBoxLayout
-from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
-
-import pyvista
 from pyvista.plotting.plotting import BasePlotter
 from pyvista.plotting.theme import rcParams
 from pyvista.utilities import conditional_decorator, threaded
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
 from .counter import Counter
 from .dialog import DoubleSlider, FileDialog, RangeGroup, ScaleAxesDialog
