c0b3f5aa1db04579d295d36fd2e1800717c3dff8
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-07-31 11:40:20 +0200

    Restore init

--- pyvistaqt/__init__.py
+++ pyvistaqt/__init__.py
@@ -1,6 +1,3 @@
 """PyVista package for 3D plotting and mesh analysis."""
 from pyvistaqt._version import __version__
-from pyvistaqt.plotting import BackgroundPlotter, QtInteractor
-from pyvistaqt.window import MainWindow
-from pyvistaqt.counter import Counter
-from pyvistaqt.dialog import FileDialog, ScaleAxesDialog
+from pyvistaqt.plotting import BackgroundPlotter, MainWindow, QtInteractor
