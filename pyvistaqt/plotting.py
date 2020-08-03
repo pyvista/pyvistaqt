5cfe48ebfffbb2f68e6b0016428bb744c38bafbd
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-08-03 17:41:49 +0200

    Make black

--- pyvistaqt/plotting.py
+++ pyvistaqt/plotting.py
@@ -44,9 +44,9 @@ import time
 import warnings
 from functools import wraps
 
-import numpy as np # type: ignore
+import numpy as np  # type: ignore
 import pyvista
-import scooby # type: ignore
+import scooby  # type: ignore
 import vtk
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import QObject, QTimer, pyqtSignal, pyqtSlot
