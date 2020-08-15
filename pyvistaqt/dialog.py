208102da1853e5ebf14bdc73cb240d38a252633e
Tetsuo Koyama <tkoyama010@gmail.com>
2020-08-15 17:56:27 +0900

    :recycle: fix by pylint

--- pyvistaqt/dialog.py
+++ pyvistaqt/dialog.py
@@ -148,11 +148,11 @@ class RangeGroup(QHBoxLayout):
         self.spinbox.valueChanged.connect(self.update_value)
         self.spinbox.valueChanged.connect(callback)
 
-    def update_spinbox(self, value):
+    def update_spinbox(self):
         """Set the value of the internal spinbox."""
         self.spinbox.setValue(self.slider.value())
 
-    def update_value(self, value):
+    def update_value(self):
         """Update the value of the internal slider."""
         # if self.spinbox.value() < self.minimum:
         #     self.spinbox.setValue(self.minimum)
