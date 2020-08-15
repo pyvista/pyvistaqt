caa5d65b645e5828ddba5e213f8eb7f800ee56ca
Tetsuo Koyama <tkoyama010@gmail.com>
2020-08-16 01:07:35 +0900

    Update pyvistaqt/plotting.py

--- pyvistaqt/plotting.py
+++ pyvistaqt/plotting.py
@@ -616,7 +616,7 @@ class BackgroundPlotter(QtInteractor):
         """
         super(BackgroundPlotter, self).reset_key_events()
         if self.allow_quit_keypress:
-            self.add_key_event("q", self.close())
+            self.add_key_event("q", lambda: self.close())
 
     def scale_axes_dialog(self, show=True):
         """Open scale axes dialog."""
