84ed9208c4d1cd5765fbe73bd3a474c1e8205f13
Eric Larson <larson.eric.d@gmail.com>
2020-08-20 12:58:59 -0400

    MAINT: Bump verison

--- pyvistaqt/_version.py
+++ pyvistaqt/_version.py
@@ -1,6 +1,6 @@
 """Version info for pyvistaqt."""
 # major, minor, patch
-version_info = 0, 1, 1
+version_info = 0, 2, 0
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
