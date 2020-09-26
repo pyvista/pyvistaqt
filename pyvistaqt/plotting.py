e9f88a857eb71eadef533697055c3a4f314f28be
Tetsuo Koyama <tkoyama010@gmail.com>
2020-09-26 21:56:57 +0900

    Fix pylint

--- pyvistaqt/plotting.py
+++ pyvistaqt/plotting.py
@@ -425,7 +425,7 @@ class QtInteractor(QVTKRenderWindowInteractor, BasePlotter):
         """Save camera position to saved camera menu for recall."""
         if self.saved_camera_positions is not None:
             # pylint: disable=attribute-defined-outside-init
-            self.camera_position : Any
+            self.camera_position: Any
             self.saved_camera_positions.append(self.camera_position)
             ncam = len(self.saved_camera_positions)
         if self.camera_position is not None:
