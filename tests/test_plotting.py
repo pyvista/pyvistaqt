6abcfbb36c1a5b0fcf6f74bc7a2f2e6b7cc9edb8
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-11-10 14:33:02 +0100

    Keep compatibility

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,5 +1,6 @@
 import os
 import platform
+from distutils.version import LooseVersion
 
 import numpy as np
 import pytest
@@ -243,6 +244,8 @@ def test_qt_interactor(qtbot):
     assert not render_timer.isActive()
 
     # check that BasePlotter.close() is called
+    if LooseVersion(pyvista.__version__) < '0.27.0':
+        assert not hasattr(vtk_widget, "iren")
     assert vtk_widget._closed
 
     # check that BasePlotter.__init__() is called only once
@@ -673,6 +676,8 @@ def test_background_plotting_close(qtbot, close_event, empty_scene):
     assert not render_timer.isActive()
 
     # check that BasePlotter.close() is called
+    if LooseVersion(pyvista.__version__) < '0.27.0':
+        assert not hasattr(vtk_widget, "iren")
     assert plotter._closed
 
     # check that BasePlotter.__init__() is called only once
