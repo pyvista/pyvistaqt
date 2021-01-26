b4ae52ea10db3d92887e8f717609386143fff732
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-01-26 12:13:22 +0100

    Make isort

--- pyvistaqt/__init__.py
+++ pyvistaqt/__init__.py
@@ -1,5 +1,5 @@
 """PyVista package for 3D plotting and mesh analysis."""
 from pyvistaqt._version import __version__
-from pyvistaqt.plotting import MultiPlotter, BackgroundPlotter, MainWindow, QtInteractor
+from pyvistaqt.plotting import BackgroundPlotter, MainWindow, MultiPlotter, QtInteractor
 
-__all__ = ["__version__", "MultiPlotter", "BackgroundPlotter", "MainWindow", "QtInteractor"]
+__all__ = ["__version__", "BackgroundPlotter", "MainWindow", "MultiPlotter", "QtInteractor"]
