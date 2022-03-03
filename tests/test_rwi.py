ac644b33b73ec13b2443dc39150bd9edd21da79c
Guillaume Favelier <guillaume.favelier@gmail.com>
2022-03-03 17:14:59 +0100

    again [skip azp]

--- tests/test_rwi.py
+++ tests/test_rwi.py
@@ -1,8 +1,3 @@
-import sys
-from mne.utils import run_subprocess
-
-
 def test_rwi(qapp):
-    stdout, stderr, code = run_subprocess([sys.executable, 'pyvistaqt/rwi.py'],
-                                          return_code=True)
-    assert code == 0, stdout + stderr
+    import IPython
+    IPython.start_ipython(argv=['pyvistaqt/rwi.py'])
