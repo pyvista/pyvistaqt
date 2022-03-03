488947cf5a6795bfd24e342502566b0a86b72124
Guillaume Favelier <guillaume.favelier@gmail.com>
2022-03-03 17:06:27 +0100

    improve [skip azp]

--- tests/test_rwi.py
+++ tests/test_rwi.py
@@ -1,34 +1,8 @@
-from vtkmodules.vtkFiltersSources import vtkConeSource
-from vtkmodules.vtkRenderingCore import (vtkActor, vtkPolyDataMapper,
-                                         vtkRenderer)
-
-from qtpy.QtWidgets import QMainWindow
-from pyvistaqt.rwi import QVTKRenderWindowInteractor
+import sys
+from mne.utils import run_subprocess
 
 
 def test_rwi(qapp):
-    window = QMainWindow()
-
-    # create the widget
-    widget = QVTKRenderWindowInteractor(window)
-    window.setCentralWidget(widget)
-
-    ren = vtkRenderer()
-    widget.GetRenderWindow().AddRenderer(ren)
-
-    cone = vtkConeSource()
-    cone.SetResolution(8)
-
-    coneMapper = vtkPolyDataMapper()
-    coneMapper.SetInputConnection(cone.GetOutputPort())
-
-    coneActor = vtkActor()
-    coneActor.SetMapper(coneMapper)
-
-    ren.AddActor(coneActor)
-
-    # show the widget
-    window.show()
-
-    widget.Initialize()
-    widget.Start()
+    stdout, stderr, code = run_subprocess([sys.executable, 'pyvistaqt/rwi.py'],
+                                          return_code=True)
+    assert code == 0, stdout + stderr
