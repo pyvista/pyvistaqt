7793d2e472a35f16c3ae0460267a2bb032a34657
Guillaume Favelier <guillaume.favelier@gmail.com>
2022-03-29 23:06:21 +0200

    linting

--- pyvistaqt/__init__.py
+++ pyvistaqt/__init__.py
@@ -1,25 +1,31 @@
 """PyVista package for 3D plotting and mesh analysis."""
 from ._version import __version__
 
-
 try:
     from qtpy import QtCore  # noqa
 except ModuleNotFoundError:
+
+    # pylint: disable=too-few-public-methods,missing-class-docstring
     class _QtBindingError:
         def __init__(self, *args, **kwargs):
-            raise RuntimeError('No Qt binding is found')
+            raise RuntimeError("No Qt binding is found")
 
+    # pylint: disable=too-few-public-methods,missing-class-docstring
     class BackgroundPlotter(_QtBindingError):
         pass
 
+    # pylint: disable=too-few-public-methods,missing-class-docstring
     class MainWindow(_QtBindingError):
         pass
 
+    # pylint: disable=too-few-public-methods,missing-class-docstring
     class MultiPlotter(_QtBindingError):
         pass
 
+    # pylint: disable=too-few-public-methods,missing-class-docstring
     class QtInteractor(_QtBindingError):
         pass
+
 else:
     from .plotting import BackgroundPlotter, MainWindow, MultiPlotter, QtInteractor
 
