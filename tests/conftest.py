35736a3e8bf12c1e950bd22b0332594fa67942ec
Eric Larson <larson.eric.d@gmail.com>
2022-04-01 17:53:33 -0400

    FIX: Trickery

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
-import pytest
 import importlib
-import qtpy
+import sys
+
+import pytest
 from pyvista.plotting import system_supports_plotting
+import pyvistaqt
 
 NO_PLOTTING = not system_supports_plotting()
 
@@ -26,10 +28,15 @@ def plotting():
 @pytest.fixture()
 def no_qt(monkeypatch):
     """Require plotting."""
+    need_reload = False
     if _check_qt_installed():
+        need_reload = True
         monkeypatch.setenv('QT_API', 'bad_name')
-        try:
-            importlib.reload(qtpy)
-        except Exception:
-            pass
+        sys.modules.pop('qtpy')
+        importlib.reload(pyvistaqt)
+        assert 'qtpy' not in sys.modules
     yield
+    monkeypatch.undo()
+    if need_reload:
+        importlib.reload(pyvistaqt)
+        assert 'qtpy' in sys.modules
