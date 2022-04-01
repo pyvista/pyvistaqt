5e3902172c3dcd1bcef67a18e7c16a3ea8a122d6
Guillaume Favelier <guillaume.favelier@gmail.com>
2022-04-02 00:11:35 +0200

    MNT: conda forge (#157)
    * update
    
    * remove cruft
    
    * remove cruft
    
    * refactor
    
    * linting
    
    * doc
    
    * do not cover
    
    * add no qt job
    
    * fix
    
    * testing
    
    * fix
    
    * fix
    
    * Apply suggestions from code review
    
    Co-authored-by: Eric Larson <larson.eric.d@gmail.com>
    
    * fix
    
    * update
    
    * update
    
    * update
    
    * FIX: Trickery
    
    * FIX: Shhhh
    
    Co-authored-by: Eric Larson <larson.eric.d@gmail.com>

--- pyvistaqt/__init__.py
+++ pyvistaqt/__init__.py
@@ -1,6 +1,35 @@
 """PyVista package for 3D plotting and mesh analysis."""
-from pyvistaqt._version import __version__
-from pyvistaqt.plotting import BackgroundPlotter, MainWindow, MultiPlotter, QtInteractor
+from ._version import __version__
+
+try:
+    from qtpy import QtCore  # noqa
+except Exception as exc:  # pragma: no cover # pylint: disable=broad-except
+    _exc_msg = exc
+
+    # pylint: disable=too-few-public-methods
+    class _QtBindingError:
+        def __init__(self, *args, **kwargs):
+            raise RuntimeError(f"No Qt binding was found, got: {_exc_msg}")
+
+    # pylint: disable=too-few-public-methods
+    class BackgroundPlotter(_QtBindingError):
+        """Handle Qt binding error for BackgroundPlotter."""
+
+    # pylint: disable=too-few-public-methods
+    class MainWindow(_QtBindingError):
+        """Handle Qt binding error for MainWindow."""
+
+    # pylint: disable=too-few-public-methods
+    class MultiPlotter(_QtBindingError):
+        """Handle Qt binding error for MultiPlotter."""
+
+    # pylint: disable=too-few-public-methods
+    class QtInteractor(_QtBindingError):
+        """Handle Qt binding error for QtInteractor."""
+
+else:
+    from .plotting import BackgroundPlotter, MainWindow, MultiPlotter, QtInteractor
+
 
 __all__ = [
     "__version__",
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,21 @@
-import gc
-import os
+import importlib
+import sys
+
 import pytest
-import pyvista
 from pyvista.plotting import system_supports_plotting
+import pyvistaqt
 
 NO_PLOTTING = not system_supports_plotting()
+
+
+def _check_qt_installed():
+    try:
+        from qtpy import QtCore  # noqa
+    except Exception:
+        return False
+    else:
+        return True
+
 
 @pytest.fixture()
 def plotting():
@@ -12,3 +23,20 @@ def plotting():
     if NO_PLOTTING:
         pytest.skip(NO_PLOTTING, reason="Requires system to support plotting")
     yield
+
+
+@pytest.fixture()
+def no_qt(monkeypatch):
+    """Require plotting."""
+    need_reload = False
+    if _check_qt_installed():
+        need_reload = True
+        monkeypatch.setenv('QT_API', 'bad_name')
+        sys.modules.pop('qtpy')
+        importlib.reload(pyvistaqt)
+        assert 'qtpy' not in sys.modules
+    yield
+    monkeypatch.undo()
+    if need_reload:
+        importlib.reload(pyvistaqt)
+        assert 'qtpy' in sys.modules
