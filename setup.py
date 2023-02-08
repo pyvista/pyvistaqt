cdad0e984857bfc2d893f76e164daa8735241ecf
Eric Larson <larson.eric.d@gmail.com>
2023-02-08 07:20:08 -0500

    MAINT: Bump back to dev (#309)

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     version=__version__,
     description='pyvista qt plotter',
     long_description=io_open(readme_file, encoding="utf-8").read(),
+    long_description_content_type='text/x-rst',
     author='PyVista Developers',
     author_email='info@pyvista.org',
     license='MIT',
