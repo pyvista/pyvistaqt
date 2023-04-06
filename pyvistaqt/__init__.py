be2a140c36bff9edb8fdc0505d73c894e3d8db8f
Eric Larson <larson.eric.d@gmail.com>
2023-04-06 08:14:27 -0600

    FIX: Shhhh

--- pyvistaqt/__init__.py
+++ pyvistaqt/__init__.py
@@ -4,7 +4,7 @@ try:
     from importlib.metadata import version
 
     __version__ = version("mne")
-except Exception:
+except Exception:  # pylint: disable=broad-exception-caught
     try:
         from ._version import __version__
     except ImportError:
