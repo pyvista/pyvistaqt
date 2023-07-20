85b68fc4d1bc9d68488a411ac2bd943b2729ae29
Eric Larson <larson.eric.d@gmail.com>
2023-07-20 15:58:15 -0400

    FIX: Cleaner

--- docs/conf.py
+++ docs/conf.py
@@ -22,11 +22,12 @@ pyvista.OFF_SCREEN = True # Not necessary - simply an insurance policy
 pyvista.BUILDING_GALLERY = True
 # Preferred plotting style for documentation
 pyvista.set_plot_theme('document')
+ws = np.array([1024, 768]) * 2
 try:
-    rc = pyvista.global_theme
+    pyvista.global_theme.window_size = ws
 except AttributeError:
-    rc = pyvista.rcParams
-rc['window_size'] = np.array([1024, 768]) * 2
+    rc = pyvista.rcParams["window_size"] = ws
+del ws
 # Save figures in specified directory
 pyvista.FIGURE_PATH = os.path.join(os.path.abspath('./images/'), 'auto-generated/')
 if not os.path.exists(pyvista.FIGURE_PATH):
