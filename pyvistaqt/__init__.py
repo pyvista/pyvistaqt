9bbe6eb12380576272f0e40c1c14e821ee6c01cc
Eric Larson <larson.eric.d@gmail.com>
2026-07-01 21:08:45 -0400

    FIX: Simplify

--- pyvistaqt/__init__.py
+++ pyvistaqt/__init__.py
@@ -1,7 +1,5 @@
 """PyVista package for 3D plotting and mesh analysis."""  # noqa: EXE002
 
-from typing import Any
-
 try:
     from importlib.metadata import version
 
@@ -9,34 +7,10 @@ try:
 except Exception:  # pragma: no cover  # noqa: BLE001
     __version__ = "0.0.0"
 
-try:
-    from qtpy import QtCore  # noqa: F401
-except Exception as exc:  # pragma: no cover  # noqa: BLE001
-    _exc_msg = exc
-
-    class _QtBindingError:
-        def __init__(self, *args: Any, **kwargs: Any) -> None:  # noqa: ANN401, ARG002
-            msg = f"No Qt binding was found, got: {_exc_msg}"
-            raise RuntimeError(msg)
-
-    class BackgroundPlotter(_QtBindingError):  # noqa: N818
-        """Handle Qt binding error for BackgroundPlotter."""
-
-    class MainWindow(_QtBindingError):  # noqa: N818
-        """Handle Qt binding error for MainWindow."""
-
-    class MultiPlotter(_QtBindingError):  # noqa: N818
-        """Handle Qt binding error for MultiPlotter."""
-
-    class QtInteractor(_QtBindingError):  # noqa: N818
-        """Handle Qt binding error for QtInteractor."""
-
-else:
-    from .plotting import BackgroundPlotter
-    from .plotting import MainWindow
-    from .plotting import MultiPlotter
-    from .plotting import QtInteractor
-
+from .plotting import BackgroundPlotter
+from .plotting import MainWindow
+from .plotting import MultiPlotter
+from .plotting import QtInteractor
 
 __all__ = [
     "BackgroundPlotter",
